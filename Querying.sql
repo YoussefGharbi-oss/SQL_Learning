0b71f397f59a63aa6da5fb900d4318d10004bcea
YoussefGharbi-oss <youssef.gharbi@esen.tn>
2023-09-21 14:05:18 +0100

    Filtering queries

--- Querying.sql
+++ Querying.sql
@@ -1,16 +1,19 @@
-SELECT DISTINCT column1, column2, ...
+SELECT  DISTINCT column1
+       ,column2
+       ,...
 FROM table_name;
---Return How many products in the Table 
-SELECT COUNT(*)
+--Return How many products IN the TABLE 
+SELECT  COUNT(*)
 FROM Products;
---Return how many Record in the column -- 
-SELECT COUNT(column_name)
-FROM table_name 
+--Return how many Record IN the column -- 
+SELECT  COUNT(column_name)
+FROM table_name
 --COUNT MULTIPLE LINES----
-SELECT  
-   COUNT(Product_id) AS count_languages,
-   COUNT(quantity) AS count_countries   
-FROM products ; 
+SELECT  COUNT(Product_id) AS count_languages
+       ,COUNT(quantity)   AS count_countries
+FROM products
+;
+
 --COUNT DITINCT ENTREY- --
-SELECT COUNT(DISTINCT Price)
-FROM Products;
+SELECT  COUNT(DISTINCT Price)
+FROM Products; <
--- ShakeHands.sql
+++ ShakeHands.sql
@@ -1,22 +1,11 @@
---SELECT COLUMNS FROM DATA--
-SELECT column1, column2, 
+--SELECT COLUMNS
+FROM DATA--
+SELECT  column1
+       ,column2
+       ,
 FROM table_name;
---Select all Data FROM TABLE --
-SELECT * FROM table_name;
- --Select UNIQUE COLUMNS -- Inside a table, a column often contains many duplicate values; and sometimes you only want to list the different (distinct) values.
-SELECT DISTINCT column1, column2, ...
+--Select all Data
+FROM TABLE --
+SELECT  *
 FROM table_name;
---Return How many products in the Table 
-SELECT COUNT(*)
-FROM Products;
---Return how many Record in the column -- 
-SELECT COUNT(column_name)
-FROM table_name 
---COUNT MULTIPLE LINES----
-SELECT  
-   COUNT(Product_id) AS count_languages,
-   COUNT(quantity) AS count_countries   
-FROM products ; 
---COUNT DITINCT ENTREY- --
-SELECT COUNT(DISTINCT Price)
-FROM Products;
+--Select UNIQUE COLUMNS -- Inside a table, a column often contains many duplicate values; AND sometimes you only want to list the different (distinct) values.
